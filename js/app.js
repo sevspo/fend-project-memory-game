137339bb1e138b0c8f392ec17cb07d3292b00db5
Severin Spoerri <severin.spoerri@outlook.com>
2018-08-22 18:08:22 +0200

    Created icons array and cleared listelements in the HTML

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const icons = ["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt","fa fa-cube","fa fa-anchor","fa fa-leaf","fa fa-bicycle","fa fa-diamond"];
 
 /*
  * Display the cards on the page
